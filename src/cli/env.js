f6cf59991dbf61c678cd898fd11698b678f371c9
Musmen <musmen@internet.ru>
2024-10-08 00:04:52 +0300

    feat: add cli/env.js solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,23 @@
+const VARIABLE_RSS_PREFIX = 'RSS_';
+const VARIABLE_PREFIX_LENGTH = 4;
+
+const RESULT_STRING_DELIMITER = '; ';
+
+const getEnvVariablePrefix = (envVariableName) => envVariableName.slice(0, VARIABLE_PREFIX_LENGTH);
+
+const getRSSEnvVariablePairString = (envVariableName, envVariablesObject) => `${envVariableName}=${envVariablesObject[envVariableName]}`;
+
+const isRSSEnvVariable = (envVariableName) => getEnvVariablePrefix(envVariableName) === VARIABLE_RSS_PREFIX;
+
 const parseEnv = () => {
-    // Write your code here 
+  const envVariablesWithRSSPrefix = [];
+  const envVariablesObject = process.env;
+
+  for (let envVariableName in envVariablesObject) {
+    if (isRSSEnvVariable(envVariableName)) envVariablesWithRSSPrefix.push(getRSSEnvVariablePairString(envVariableName, envVariablesObject));
+  }
+
+  console.log(envVariablesWithRSSPrefix.join(RESULT_STRING_DELIMITER));
 };
 
 parseEnv();
