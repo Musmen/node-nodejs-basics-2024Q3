329095b43f8f7c27e79e6f3c0e07a1f92b0876f1
Musmen <musmen@internet.ru>
2024-10-08 14:09:27 +0300

    feat: add cli/args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
+const FIRST_ARGUMENT_INDEX = 2;
+const VARIABLE_PREFIX = '--';
+
+const isVariable = (argument) => argument.startsWith(VARIABLE_PREFIX);
+
+const getVariableValueFromArguments = (args, index) => {
+  if ((args.length <= index + 1) || isVariable(args[index + 1])) return '';
+  return args[index + 1];
+}
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(FIRST_ARGUMENT_INDEX);
+  const result = [];
+
+  args.forEach((argument, index) => {
+    if (isVariable(argument)) result.push(`${argument} is ${getVariableValueFromArguments(args, index)}`);
+  });
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
